6861c74cb280a4d0cca2eaeea9a311be18a79d37
rohan-shewale <shewale.rohan1099@gmail.com>
2023-07-10 16:16:21 +0530

    Booking Update

--- busportal/src/Components/Home/Booking.js
+++ busportal/src/Components/Home/Booking.js
@@ -11,105 +11,52 @@ export default function Booking() {
   const fees = searchParams.get("fees");
 
   return (
-    // <>
-    //   <div className='row d-flex justify-content-center align-items-center mt-5'>
-    //     <div className='shadow p-3 mb-5 bg-body-tertiary rounded col-sm-12 col-md-5 '>
-    //       <h3 className="text-center text-danger ">... Booking Details ...</h3>
-
-    //       <div className='p-3 m-3 mt-5'>
-    //         <div className='row-sm-12 row-md-5 mb-3 ms-5' style={{ width: "30vw", alignItems: "center", boxShadow: "5px 5px 20px rgb(138, 37, 64)" }}>
-    //           <div className='col fs-5 fw-bold ms-5'>Name :</div>
-    //           <div className='col me-1'>{source}</div>
-    //         </div>
-    //         <div className='row-sm-12 mb-3 ms-5' style={{ width: "30vw", alignItems: "center", boxShadow: "5px 5px 20px rgb(138, 37, 64)" }}>
-    //           <div className='col fs-5 fw-bold ms-5'>Bus Id :</div>
-    //           <div className='col me-1'>{busId}</div>
-    //         </div>
-    //         <div className='row-sm-12 mb-3 ms-5' style={{ width: "30vw", alignItems: "center", boxShadow: "5px 5px 20px rgb(138, 37, 64)" }}>
-    //           <div className='col fs-5 fw-bold ms-5'>Seat capacity :</div>
-    //           <div className='col me-1'>{seatCapacity}</div>
-    //         </div>
-    //         <div className='row-sm-12 mb-3 ms-5' style={{ width: "30vw", alignItems: "center", boxShadow: "5px 5px 20px rgb(138, 37, 64)" }}>
-    //           <div className='col fs-5 fw-bold ms-5'>From :</div>
-    //           <div className='col me-1'>MGM Hodpital</div>
-    //         </div>
-    //         <div className='row-sm-12 mb-3 ms-5' style={{ width: "30vw", alignItems: "center", boxShadow: "5px 5px 20px rgb(138, 37, 64)" }}>
-    //           <div className='col fs-5 fw-bold ms-5'>Fees :</div>
-    //           <div className='col me-1'>Rs. 5000</div>
-    //         </div>
-    //         <div className='row-sm-12 mb-3 ms-5' style={{ width: "30vw", alignItems: "center", boxShadow: "5px 5px 20px rgb(138, 37, 64)" }}>
-    //           <div className='col fs-5 fw-bold ms-5'>Fees Status :</div>
-    //           <div className='col me-1'>Paid</div>
-    //         </div>
-
-    //         <div className='row'>
-    //           {/* <div className='col-sm-12 col-md-5'>
-    //             <div className="mt-5 ">
-    //               <button className="mt-2 ms-5 btn btn-danger" type="submit">
-    //                 Book Now
-    //               </button>
-    //             </div>
-    //           </div> */}
-    //           <div className='col'>
-    //             <Link to={`/payfees?bus_id=${busId}&fees=${fees}`}>
-    //               <div className="mt-5">
-    //                 <button className="mt-2 ms-5 btn btn-danger" style={{ margin: '20px' }} type="submit">
-    //                   Pay Fees
-    //                 </button>
-    //               </div>
-    //             </Link>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </>
     <>
-  <div className='row d-flex justify-content-center align-items-center mt-5'>
-    <div className='shadow p-3 mb-5 bg-body-tertiary rounded col-sm-12 col-md-5'>
-      <h3 className="text-center text-danger">... Booking Details ...</h3>
+      <div className='row d-flex justify-content-center align-items-center mt-5'>
+        <div className='shadow p-3 mb-5 bg-body-tertiary rounded col-sm-12 col-md-5'>
+          <h3 className="text-center text-danger">... Booking Details ...</h3>
 
-      <div className='p-3 m-3 mt-5'>
-        <div className='row mb-3' style={{ alignItems: "center", boxShadow: "5px 5px 20px rgb(138, 37, 64)" }}>
-          <div className='col-4 fs-5 fw-bold'>Name :</div>
-          <div className='col-8'>{source}</div>
-        </div>
-        <div className='row mb-3' style={{ alignItems: "center", boxShadow: "5px 5px 20px rgb(138, 37, 64)" }}>
-          <div className='col-4 fs-5 fw-bold'>Bus Id :</div>
-          <div className='col-8'>{busId}</div>
-        </div>
-        <div className='row mb-3' style={{ alignItems: "center", boxShadow: "5px 5px 20px rgb(138, 37, 64)" }}>
-          <div className='col-4 fs-5 fw-bold'>Seat capacity :</div>
-          <div className='col-8'>{seatCapacity}</div>
-        </div>
-        <div className='row mb-3' style={{ alignItems: "center", boxShadow: "5px 5px 20px rgb(138, 37, 64)" }}>
-          <div className='col-4 fs-5 fw-bold'>From :</div>
-          <div className='col-8'>MGM Hospital</div>
-        </div>
-        <div className='row mb-3' style={{ alignItems: "center", boxShadow: "5px 5px 20px rgb(138, 37, 64)" }}>
-          <div className='col-4 fs-5 fw-bold'>Fees :</div>
-          <div className='col-8'>Rs. 5000</div>
-        </div>
-        <div className='row mb-3' style={{ alignItems: "center", boxShadow: "5px 5px 20px rgb(138, 37, 64)" }}>
-          <div className='col-4 fs-5 fw-bold'>Fees Status :</div>
-          <div className='col-8'>Paid</div>
-        </div>
+          <div className='p-3 m-3 mt-5'>
+            <div className='row mb-3' style={{ alignItems: "center", boxShadow: "5px 5px 20px rgb(138, 37, 64)" }}>
+              <div className='col-4 fs-5 fw-bold'>Name :</div>
+              <div className='col-8'>{source}</div>
+            </div>
+            <div className='row mb-3' style={{ alignItems: "center", boxShadow: "5px 5px 20px rgb(138, 37, 64)" }}>
+              <div className='col-4 fs-5 fw-bold'>Bus Id :</div>
+              <div className='col-8'>{busId}</div>
+            </div>
+            <div className='row mb-3' style={{ alignItems: "center", boxShadow: "5px 5px 20px rgb(138, 37, 64)" }}>
+              <div className='col-4 fs-5 fw-bold'>Seat capacity :</div>
+              <div className='col-8'>{seatCapacity}</div>
+            </div>
+            <div className='row mb-3' style={{ alignItems: "center", boxShadow: "5px 5px 20px rgb(138, 37, 64)" }}>
+              <div className='col-4 fs-5 fw-bold'>From :</div>
+              <div className='col-8'>MGM Hospital</div>
+            </div>
+            <div className='row mb-3' style={{ alignItems: "center", boxShadow: "5px 5px 20px rgb(138, 37, 64)" }}>
+              <div className='col-4 fs-5 fw-bold'>Fees :</div>
+              <div className='col-8'>Rs. 5000</div>
+            </div>
+            <div className='row mb-3' style={{ alignItems: "center", boxShadow: "5px 5px 20px rgb(138, 37, 64)" }}>
+              <div className='col-4 fs-5 fw-bold'>Fees Status :</div>
+              <div className='col-8'>Paid</div>
+            </div>
 
-        <div className='row'>
-          <div className='col'>
-            <Link to={`/payfees?bus_id=${busId}&fees=${fees}`}>
-              <div className="mt-5">
-                <button className="mt-2 btn btn-danger" style={{ margin: '20px' }} type="submit">
-                  Pay Fees
-                </button>
+            <div className='row'>
+              <div className='col'>
+                <Link to={`/payfees?bus_id=${busId}&fees=${fees}`}>
+                  <div className="mt-5">
+                    <button className="mt-2 btn btn-danger" style={{ margin: '20px' }} type="submit">
+                      Pay Fees
+                    </button>
+                  </div>
+                </Link>
               </div>
-            </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-</>
+    </>
 
   );
 }
